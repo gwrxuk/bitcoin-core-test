3bddd5e9476609ed6006025cc417d6aa97d4ab9c
gwrxuk <gwrx2005@gmail.com>
2018-08-05 07:29:33 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+var bitcoin = require('bitcoin');
+var client = new bitcoin.Client({
+  host: 'localhost',
+  port: 8332,
+  user: 'admin',
+  pass: 'admin'
+});
+
+client.getBlockchainInfo(function(err, info) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('Info: ' + info);
+});
+
