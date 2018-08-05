e31280db0477f89b878d67171af73d50dbe1c942
gwrxuk <gwrx2005@gmail.com>
2018-08-06 07:05:29 +0800

    Update coin.js

--- coin.js
+++ coin.js
@@ -1,1 +1,7 @@
 const bitcoin = require("bitcoinjs-lib");
+function rng () { return Buffer.from('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz') }
+
+const keyPair = bitcoin.ECPair.makeRandom({ rng: rng })
+const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
+console.log(keyPair);
+console.log(address);
