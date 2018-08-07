c9e4f0b59ac2724cb7ea7708d1f381fd0c7d3f5f
gwrxuk <gwrx2005@gmail.com>
2018-08-07 18:25:58 +0800

    Update p2sh.js

--- p2sh.js
+++ p2sh.js
@@ -25,3 +25,28 @@ const bip65 = require('bip65')
     const { address } = bitcoin.payments.p2sh({
       redeem: bitcoin.payments.p2ms({ m: 2, pubkeys })
     })
+
+    //輸入私鑰，教學用，請勿隨意公開私鑰
+var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";
+
+//產生公鑰跟私鑰
+const keyPair = bitcoin.ECPair.fromWIF(privateKey,regtest);
+
+//產生付款位址
+const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
+
+const txb = new bitcoin.TransactionBuilder(regtest)
+
+
+//用listunspent 取出最後一筆資料的txid
+txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
+
+//用getnewaddress取得新的位址
+txb.addOutput(address, 12)
+
+//交易簽名
+txb.sign(0, keyPair); //第一個位置的是上一筆交易中的第一個支出，第二個欄位是我們的公鑰與私鑰
+
+//取得交易序號
+const transaction_01 = txb.build().toHex(); 
+console.log(transaction_01);
