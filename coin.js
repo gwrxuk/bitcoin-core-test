957d8b7da5f3c97d921a3e4accf0b2746906b9af
gwrxuk <gwrx2005@gmail.com>
2018-08-07 14:15:03 +0800

    Update coin.js

--- coin.js
+++ coin.js
@@ -8,20 +8,24 @@ const regtest = bitcoin.networks.testnet
 //https://github.com/bitcoinjs/bip65 
 const bip65 = require('bip65')
 
-//輸入私鑰
+//輸入私鑰，教學用，請勿隨意公開私鑰
 var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";
-console.log('a');
+
+//產生公要跟私鑰
 const keyPair = bitcoin.ECPair.fromWIF(privateKey,regtest);
-console.log('b');
+
 //產生付款位址
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
 
 const txb = new bitcoin.TransactionBuilder(regtest)
-console.log(txb);
+
 
 //用listunspent 取出最後一筆資料的txid
 txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
 
 //用getnewaddress取得新的位址
 txb.addOutput('2MxfNS8sB1GcCo3GBLmX2PUw277H9zvrn2L', 12)
+
+//交易簽名
 txb.sign(0, keyPair)
+console.log(txb.build().toHex());
