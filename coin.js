128171c5dc61dc92305438fcead336cdfc596ba8
gwrxuk <gwrx2005@gmail.com>
2018-08-07 10:44:36 +0800

    Update coin.js

--- coin.js
+++ coin.js
@@ -9,5 +9,9 @@ const bip65 = require('bip65')
 const keyPair = bitcoin.ECPair.makeRandom()
 
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
+
+//Bitcoin皮夾匯入格式
+const wif = keyPair.toWIF()
 console.log(keyPair);
-console.log(address);
+console.log(bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey }));
+console.log(wif);
