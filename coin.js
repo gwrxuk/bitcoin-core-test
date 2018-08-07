a25ba93c0027acac3542b92c74a73eac982f1ca5
gwrxuk <gwrx2005@gmail.com>
2018-08-07 11:50:51 +0800

    Update coin.js

--- coin.js
+++ coin.js
@@ -1,17 +1,25 @@
+//引入bitcoin模組
 const bitcoin = require("bitcoinjs-lib");
-//function rng () { return Buffer.from('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz') }
-//const keyPair = bitcoin.ECPair.makeRandom({ rng: rng })
 
 
+//選用regtest網路
 const regtest = bitcoin.networks.testnet
+
+//https://github.com/bitcoinjs/bip65 
 const bip65 = require('bip65')
 
+//產生公鑰跟私鑰
 const keyPair = bitcoin.ECPair.makeRandom()
 
+//產生付款位址
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
 
-//Bitcoin皮夾匯入格式
-const wif = keyPair.toWIF()
-console.log(keyPair);
-console.log(bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey }));
-console.log(wif);
+const txb = new bitcoin.TransactionBuilder()
+
+txb.setVersion(1);
+//用listunspent 取出最後一筆資料的txid
+txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
+
+//用getnewaddress取得新的位址
+txb.addOutput('2MxfNS8sB1GcCo3GBLmX2PUw277H9zvrn2L', 12000)
+txb.sign(0, keyPair)
